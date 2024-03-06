23378175bdcae595c271e69a78ff100015bb0ddd
jacobhogrefe <jhogrefe03@gmail.com>
2024-03-06 17:57:21 -0500

    renamed script to indicate it compiles only for mac, and adds necessary external files to it

---
+++ compile_mac.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# compile python script from spec file
+pyinstaller --noconfirm AITracker-DataCollection.spec
+
+# save current directory and remove .app file
+directory=$(PWD)
+cd dist
+rm -R "DataCollection.app/"
+
+# copy assets and quarantine script into the compiled app directory
+cp -R "$directory"/assets/. "./AITracker-DataCollection/assets"
+cp "$directory/quarantine.sh" "AITracker-DataCollection/"
