8b0eb00b1439eae54a1eb704d302be4eeec25fd1
jacobhogrefe <jhogrefe03@gmail.com>
2024-03-06 23:25:54 -0500

    added info.plist for macos, app runs flawlessly on other macs

--- compile_mac.sh
+++ compile_mac.sh
@@ -11,3 +11,4 @@ rm -R "DataCollection.app/"
 # copy assets and quarantine script into the compiled app directory
 cp -R "$directory"/assets/. "./AITracker-DataCollection/assets"
 cp "$directory/quarantine.sh" "AITracker-DataCollection/"
+cp "$directory/Info.plist" "AITracker-DataCollection/"
